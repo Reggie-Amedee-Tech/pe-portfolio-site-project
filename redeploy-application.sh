6220aac12277f34b0690533954200a948b1541a0
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2023-08-17 14:31:16 -0400

    changed script file

--- redeploy-application.sh
+++ redeploy-application.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd ~/pe-portfolio-site-project
+cd pe-portfolio-site-project
 
 git fetch && git reset origin/main --hard
 
 source python3-virtualenv/bin/activate
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 docker compose -f docker-compose.prod.yml down 
 
